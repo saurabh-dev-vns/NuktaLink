f275bec7a3a069b21cd4dd6d8865d51257323a48
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-06 22:20:40 +0530

    FormCard modified

--- client/src/components/Cards/formCard.jsx
+++ client/src/components/Cards/formCard.jsx
@@ -7,7 +7,7 @@ const FormCard = () => {
   const handleUrlSubmit = async (e) => {
     e.preventDefault(); // Prevent default form submission
     try {
-      const response = await axios.post('https://nukta-link.vercel.app/shorten', {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/shorten`, {
         originalUrl: OriginalURL
       });
       console.log(response.data);
