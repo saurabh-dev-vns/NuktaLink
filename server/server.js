a83a780a156d73b17cb7f8161fa83f8acf92f139
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-06 19:08:47 +0530

    cors error solved

--- server/server.js
+++ server/server.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const connectDB = require("./config/db_config");
 const urlRoutes = require('./routes/urlRoutes');
-
+const cors = require('cors');
 const app = express();
+
+//handle cors
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,
+}));
+
 //server port
 const PORT = process.env.PORT || 5050;
 
