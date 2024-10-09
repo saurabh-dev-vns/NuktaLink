5dd6718c9a28a23769dfc27c28862189f4e80434
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-09 21:11:06 +0530

    Updated

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import FormCard from "../../components/Cards/FormCard";
-
 
 const Home = () => {
   return (
     <>
     <main className="flex w-full ml-5 mr-5 mt-10 h-full min-w-0 justify-center border-box top-20 relative z-0">
-       <FormCard/>
+      
       </main>
     </>
   );
